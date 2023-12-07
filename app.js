5d191be3e50f23c3191380e0970324095d6a2517
Lucas Vasquez <lucas.vasquezfc@gmail.com>
2023-12-07 13:50:05 -0300

    animais-fantasticos
    construindo animais fantasticos page para treinar a manipulação do DOM

--- app.js
+++
@@ -1,9 +0,0 @@
-const href = location.href;
-const ativo = document.querySelector('.ativo');
-const langNav = navigator.language;
-const larguraTela = window.innerWidth;
-
-console.log(href);
-console.log(larguraTela);
-console.log(langNav);
-console.log(ativo.innerText);
---
+++ exercicio-dom-1/app.js
@@ -0,0 +1,5 @@
+const href = location.href;
+const ativo = document.querySelector('.ativo');
+const langNav = navigator.language;
+
+console.log(href, ativo, langNav, innerWidth);
