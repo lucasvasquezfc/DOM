a3f2e85d43dca132e77656393735d33e73a9e941
Lucas Vasquez <lucas.vasquezfc@gmail.com>
2023-12-07 13:14:36 -0300

    iniciando

---
+++ app.js
@@ -0,0 +1,9 @@
+const href = location.href;
+const ativo = document.querySelector('.ativo');
+const langNav = navigator.language;
+const larguraTela = window.innerWidth;
+
+console.log(href);
+console.log(larguraTela);
+console.log(langNav);
+console.log(ativo.innerText);
