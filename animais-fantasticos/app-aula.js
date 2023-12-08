704750c4ec583a3ac6dd04569d34c41f602d03aa
Lucas Vasquez <lucas.vasquezfc@gmail.com>
2023-12-08 01:17:17 -0300

    update

---
+++ animais-fantasticos/app-aula.js
@@ -0,0 +1,28 @@
+// const animaisImg = document.querySelectorAll('.animais img');
+// console.log(animaisImg);
+
+// const todasImagens = document.querySelectorAll('img');
+// console.log(todasImagens);
+
+// const imagem = document.querySelectorAll('img[src^="img/imagem"]');
+// console.log(imagem);
+
+// const linksInternos = document.querySelectorAll('a[href^="#"]');
+// console.log(linksInternos);
+
+// const h2 = document.querySelector('.animais-descricao h2');
+// console.log(h2.innerHTML); // const p = document.querySelector('footer p');
+
+// const p = document.querySelectorAll('p');
+
+// console.log(p[--p.length]);
+
+// const imgs = document.querySelectorAll('img');
+
+// imgs.forEach((item, index) => {
+//   console.log(item, index);
+// });
+
+// const p = document.querySelectorAll('p');
+
+// p.forEach((item) => console.log(item.innerHTML));
--- animais-fantasticos/app.js
+++ animais-fantasticos/app.js
@@ -1,18 +1,6 @@
-// const animaisImg = document.querySelectorAll('.animais img');
-// console.log(animaisImg);
+const menu = document.querySelector('.menu');
 
-// const todasImagens = document.querySelectorAll('img');
-// console.log(todasImagens);
+menu.classList.add('ativo');
+menu.classList.remove('azul');
 
-// const imagem = document.querySelectorAll('img[src^="img/imagem"]');
-// console.log(imagem);
-
-// const linksInternos = document.querySelectorAll('a[href^="#"]');
-// console.log(linksInternos);
-
-// const h2 = document.querySelector('.animais-descricao h2');
-// console.log(h2.innerHTML); // const p = document.querySelector('footer p');
-
-// const p = document.querySelectorAll('p');
-
-// console.log(p[--p.length]);
+console.log(menu.classList);
