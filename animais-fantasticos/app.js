0cac8b4a2417ba4515476939dad4a5c0464dff05
Lucas Vasquez <lucas.vasquezfc@gmail.com>
2023-12-08 00:40:07 -0300

    update exercicio

--- animais-fantasticos/app.js
+++ animais-fantasticos/app.js
@@ -1,4 +1,18 @@
-const animais = document.getElementById('animais');
-console.log('animais');
+// const animaisImg = document.querySelectorAll('.animais img');
+// console.log(animaisImg);
 
-console.log('================================');
+// const todasImagens = document.querySelectorAll('img');
+// console.log(todasImagens);
+
+// const imagem = document.querySelectorAll('img[src^="img/imagem"]');
+// console.log(imagem);
+
+// const linksInternos = document.querySelectorAll('a[href^="#"]');
+// console.log(linksInternos);
+
+// const h2 = document.querySelector('.animais-descricao h2');
+// console.log(h2.innerHTML); // const p = document.querySelector('footer p');
+
+// const p = document.querySelectorAll('p');
+
+// console.log(p[--p.length]);
