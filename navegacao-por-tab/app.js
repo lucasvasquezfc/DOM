73ed71707a0060bd57cc072c7b1bcf53169093f0
Lucas Vasquez <lucas.vasquezfc@gmail.com>
2023-12-09 13:08:04 -0300

    navegação por tab

---
+++ navegacao-por-tab/app.js
@@ -0,0 +1,24 @@
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
+const tabContent = document.querySelectorAll('.js-tabcontent section');
+
+if (tabMenu.length && tabContent.length) {
+  tabContent[0].classList.add('ativo');
+  tabMenu[0].classList.add('ativo');
+
+  function activeTab(index) {
+    tabContent.forEach((item) => {
+      item.classList.remove('ativo');
+    });
+    tabMenu.forEach((item) => {
+      item.classList.remove('ativo');
+    });
+    tabContent[index].classList.add('ativo');
+    tabMenu[index].classList.add('ativo');
+  }
+
+  tabMenu.forEach((item, index) => {
+    item.addEventListener('click', () => {
+      activeTab(index);
+    });
+  });
+}
