3998d8087d963dafd764a0e7582ead6a2330393f
Lucas Vasquez <lucas.vasquezfc@gmail.com>
2023-12-13 14:43:25 -0300

    menu-mobile

---
+++ efeitos-no-dom/menu-mobile/js/modules/tab-nav.js
@@ -0,0 +1,27 @@
+export default function initTabNav() {
+  const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
+  const tabContent = document.querySelectorAll('[data-tab="content"] section');
+  const activeClass = 'ativo';
+
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add(activeClass);
+    tabMenu[0].classList.add(activeClass);
+
+    function activeTab(index) {
+      tabContent.forEach((item) => {
+        item.classList.remove(activeClass);
+      });
+      tabMenu.forEach((item) => {
+        item.classList.remove(activeClass);
+      });
+      tabContent[index].classList.add(activeClass);
+      tabMenu[index].classList.add(activeClass);
+    }
+
+    tabMenu.forEach((item, index) => {
+      item.addEventListener('click', () => {
+        activeTab(index);
+      });
+    });
+  }
+}
