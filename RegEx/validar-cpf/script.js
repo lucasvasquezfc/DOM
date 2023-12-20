de40eaacfd1bb55694258c23e9e554b10db8fdb2
Lucas Vasquez <lucas.vasquezfc@gmail.com>
2023-12-20 15:05:52 -0300

    update

--- RegEx/validar-cpf/script.js
+++ RegEx/validar-cpf/script.js
@@ -3,4 +3,8 @@ import ValidarCPF from './validar-cpf.js';
 const cpf = document.querySelector('#cpf');
 const validarCPF = new ValidarCPF(cpf).iniciar();
 
+const validadorCPF = new ValidarCPF();
+
+console.log(validadorCPF.formatar('111 111 111 33'));
+
 console.log(validarCPF);
