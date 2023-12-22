939f2b7a3d473eea004836d773ee734774dcb622
lucas <lucas.vasquezfc@gmail.com>
2023-12-22 13:52:38 -0300

    prejeto final de js

---
+++ projeto-final/animais-fantasticos/js/modules/fetch-bitcoin.js
@@ -0,0 +1,21 @@
+export default function initFetchBitcoin() {}
+const preco = document.querySelector('.btc-preco');
+
+function fetchBtc(url) {
+  fetch(url)
+    .then((r) => r.json())
+    .then((body) => {
+      const reais = 100 / body.BRL.buy;
+      // const formatar = new Intl.NumberFormat('pt-BR', {
+      //   style: 'currency',
+      //   currency: 'BRL',
+      // });
+      // preco.innerText = formatar.format(reais);
+      preco.innerText = reais.toFixed(6);
+    })
+    .catch((erro) => {
+      console.log(Error(erro));
+    });
+}
+
+fetchBtc('https://www.blockchain.com/pt/ticker');
